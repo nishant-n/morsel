20f41fb34a8b85046651c3f290a3d5852dec627b
Nishant <nishant.n@cisinlabs.com>
2015-03-23 14:03:40 +0530

    change css path to localhost

--- embed.js
+++ embed.js
@@ -33,7 +33,8 @@
 	var fileref=document.createElement("link");
 	fileref.setAttribute("rel", "stylesheet");
 	fileref.setAttribute("type", "text/css");
-	fileref.setAttribute("href", "https://raw.githubusercontent.com/nishant-n/morsel/master/embed.css");
+	//fileref.setAttribute("href", "https://raw.githubusercontent.com/nishant-n/morsel/master/embed.css");
+	fileref.setAttribute("href", "localhost/demo/embed.css");
 	document.getElementsByTagName("head")[0].appendChild(fileref) //
 
   var items = result.data.items;
