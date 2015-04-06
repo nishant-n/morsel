93de7e5c1f4fde647cec68438be3833a23072100
Nishant <nishant.n@cisinlabs.com>
2015-04-06 20:16:48 +0530

    load all js and css changes

--- embed.js
+++ embed.js
@@ -1,3 +1,67 @@
+function timeAgo(selector) {
+
+    var templates = {
+        prefix: "",
+        suffix: " ago",
+        seconds: "less than a minute",
+        minute: "about a minute",
+        minutes: "%d minutes",
+        hour: "about an hour",
+        hours: "about %d hours",
+        day: "a day",
+        days: "%d days",
+        month: "about a month",
+        months: "%d months",
+        year: "about a year",
+        years: "%d years"
+    };
+    var template = function(t, n) {
+        return templates[t] && templates[t].replace(/%d/i, Math.abs(Math.round(n)));
+    };
+
+    var timer = function(time) {
+        if (!time)
+            return;
+        time = time.replace(/\.\d+/, ""); // remove milliseconds
+        time = time.replace(/-/, "/").replace(/-/, "/");
+        time = time.replace(/T/, " ").replace(/Z/, " UTC");
+        time = time.replace(/([\+\-]\d\d)\:?(\d\d)/, " $1$2"); // -04:00 -> -0400
+        time = new Date(time * 1000 || time);
+
+        var now = new Date();
+        var seconds = ((now.getTime() - time) * .001) >> 0;
+        var minutes = seconds / 60;
+        var hours = minutes / 60;
+        var days = hours / 24;
+        var years = days / 365;
+
+        return templates.prefix + (
+                seconds < 45 && template('seconds', seconds) ||
+                seconds < 90 && template('minute', 1) ||
+                minutes < 45 && template('minutes', minutes) ||
+                minutes < 90 && template('hour', 1) ||
+                hours < 24 && template('hours', hours) ||
+                hours < 42 && template('day', 1) ||
+                days < 30 && template('days', days) ||
+                days < 45 && template('month', 1) ||
+                days < 365 && template('months', days / 30) ||
+                years < 1.5 && template('year', 1) ||
+                template('years', years)
+                ) + templates.suffix;
+    };
+
+    var elements = document.getElementsByClassName('timeago');
+    
+    for (var i in elements) {
+        var $this = elements[i];
+        if (typeof $this === 'object') {
+            $this.innerHTML = timer($this.getAttribute('title') || $this.getAttribute('datetime'));
+        }
+    }
+    // update time every minute
+    //setTimeout(timeAgo, 60000);
+
+}
 // Anonymous "self-invoking" function
 (function() {
     
@@ -34,15 +98,16 @@
 	function genrateResponse(xhr,val,status){
 
 	 	var result = JSON.parse(xhr.response);
+
 		var fileref=document.createElement("link");
 		fileref.setAttribute("rel", "stylesheet");
 		fileref.setAttribute("type", "text/css");
 		fileref.setAttribute("href", "https://rawgit.com/nishant-n/morsel/staging/embed.css");	
 		document.getElementsByTagName("head")[0].appendChild(fileref);
-
 	  	
 	  	var html = '';
-		html += '<div class="page-wrapper page-wrapper-details" style="overflow-y:auto;height:auto;margin:auto;"  onclick="window.open(\''+val+'\',\'_blank\')">';
+		//html += '<div class="page-wrapper page-wrapper-details" style="overflow-y:auto;height:auto;margin:auto;"  onclick="window.open(\''+val+'\',\'_blank\')">';1
+		html += '<div class="page-wrapper page-wrapper-details" style="overflow-y:auto;height:auto;margin:auto;">';
 		
 		if(status){
 			
@@ -50,13 +115,13 @@
 			html +=      '<div class="modal-morsel-full-slide" >';
 			html +=          '<div class="morsel-full">';
 			html +=                '<div class="morsel-mobile-info" style="background-image:url(\''+result.data.primary_item_photos._320x320+'\')">';
-			html +=   '<h2 bo-text="morsel.title" class="morsel-title">'+result.data.title+'</h2>';
+			html +=   '<h2 bo-text="morsel.title" class="morsel-title"><a href="'+val+'" target="_blank">'+result.data.title+'</a></h2>';
 			html +=   '<div class="user">\
 			              <span class="profile-pic-link profile-pic-xs">';
-			                 html += '<img class="img-circle"  src="'+result.data.creator.photos._40x40+'">';
-			             html += '</span>\
-			               <br/>'+result.data.creator.first_name+' '+result.data.creator.last_name+'\
-			          </div>\
+			            html += '<img class="img-circle"  src="'+result.data.creator.photos._40x40+'"></span>';
+			            html += '<br/><a href="https://www.eatmorsel.com/'+result.data.creator.username+'">'+result.data.creator.first_name+' '+result.data.creator.last_name+'</a>';
+			            html += '<br/><span class="place-info">'+result.data.place.name+' '+result.data.place.city+' '+result.data.place.state+'</span><br/><abbr class="timeago time-info" title="'+result.data.created_at+'"></abbr>\
+			            </div>\
 			        </div>';
 
 						/*for(index in items)
@@ -92,6 +157,7 @@
 
 		var wrapper = document.getElementById('morsel-embed-wrapper');		
 	  	wrapper.innerHTML= html;
+	  	timeAgo();
 	}
 	loadMorsel();
 })();
