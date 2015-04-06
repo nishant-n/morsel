3e1b3c5a9981f1f06d9a0bc3f8aae450caf1061a
Nishant <nishant.n@cisinlabs.com>
2015-04-06 14:47:47 +0530

    new design

--- embed.js
+++ embed.js
@@ -37,29 +37,29 @@
 		var fileref=document.createElement("link");
 		fileref.setAttribute("rel", "stylesheet");
 		fileref.setAttribute("type", "text/css");
-		fileref.setAttribute("href", "https://rawgit.com/nishant-n/morsel/master/embed.css");	
+		fileref.setAttribute("href", "http://localhost/demo/embed.css");	
 		document.getElementsByTagName("head")[0].appendChild(fileref) //
 
 	  	
 	  	var html = '';
-		html += '<div class="page-wrapper page-wrapper-details" style="overflow-y:auto;height:100%;margin:auto;"  onclick="window.open(\''+val+'\',\'_blank\')">';
+		html += '<div class="page-wrapper page-wrapper-details" style="overflow-y:auto;height:auto;margin:auto;"  onclick="window.open(\''+val+'\',\'_blank\')">';
 		
 		if(status){
-			var items = result.data.items;
-			consol.log(result.data);
+			console.log(result.data);
+			var items = result.data.items;			
 			html +=      '<div class="modal-morsel-full-slide" >';
 			html +=          '<div class="morsel-full">';
-			html +=                '<div class="morsel-mobile-info">';
+			html +=                '<div class="morsel-mobile-info" style="background-image:url(\''+result.data.primary_item_photos._320x320+'\')">';
 			html +=   '<h2 bo-text="morsel.title" class="morsel-title">'+result.data.title+'</h2>';
 			html +=   '<div class="user">\
 			              <span class="profile-pic-link profile-pic-xs">';
 			                 html += '<img class="img-circle"  src="'+result.data.creator.photos._40x40+'">';
 			             html += '</span>\
-			               '+result.data.creator.first_name+' '+result.data.creator.last_name+'\
+			               <br/>'+result.data.creator.first_name+' '+result.data.creator.last_name+'\
 			          </div>\
 			        </div>';
 
-						for(index in items)
+						/*for(index in items)
 						{
 						  html +='<div class="slide-item morsel-item " >\
 						  <hr>\
@@ -76,7 +76,7 @@
 					html +='</div>\
 							</div>\
 						</div>';
-						}
+						}*/
 		    html += '</div>\
 		    </div>\
 			</div>';	
