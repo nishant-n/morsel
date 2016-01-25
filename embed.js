8cc16334db3f22db927b9887841d4f722ff2ffda
Nishant Nigam <nishant.n@cisinlabs.com>
2016-01-25 23:20:33 +0530

    change altrenate domain https://morsel-api.herokuapp.com/

--- embed.js
+++ embed.js
@@ -6,7 +6,8 @@
 		var val =  document.getElementById("morsel-embed").href   
  		var post_name = val.substr(val.lastIndexOf('/') + 1);
 		var xmlhttp;
-		var url ="https://api.eatmorsel.com/morsels/";
+		//var url ="https://api.eatmorsel.com/morsels/";
+		var url ="https://morsel-api.herokuapp.com/morsels/";
 		if (window.XMLHttpRequest)
 		{// code for IE7+, Firefox, Chrome, Opera, Safari
 			xmlhttp = new XMLHttpRequest();
